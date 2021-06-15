f98819b71a44df1382b72611d6eb09614b9291fe
Dmitriy <example@mail.com>
2021-06-15 13:52:20 +0300

    3. шифрование в верхнем и нижнем регистре

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 function rot13(message) {
    const alphabetLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
    const alphabetUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-   let arrOfIndex = []  // массив чисел, соответствующих индексам в массиве alphabetLow/alphabetUp
-   let arrOfIndexPlusNum  // те же числа но увеличенные на заданное число
    let rotArr = []  //  числа из массива arrOfIndexPlusNum, преобразованные в буквы из alphabetLow
-   // Функция по возврату массива arrOfIndexPlusNum
-   
-   message.split("").forEach(char => {
-      let charIndex = alphabetLow.findIndex(item => item === char)
+   function someFunction(alphabet, elem) {
+      let arrOfIndex = []  // массив чисел, соответствующих индексам в массиве alphabetLow/alphabetUp
+      let arrOfIndexPlusNum  // те же числа но увеличенные на заданное число
+      let charIndex = alphabet.findIndex(item => item === elem)
       arrOfIndex.push(charIndex)
       arrOfIndexPlusNum = arrOfIndex.map(index => {
-         if((index + 13) > alphabetLow.length) {
-            return (index + 13) - alphabetLow.length
+         if ((index + 13) > alphabet.length) {
+            return (index + 13) - alphabet.length
          }
          return index + 13
       })
-   });
-   console.log(arrOfIndexPlusNum);
-   arrOfIndexPlusNum.forEach(elem => {
-      alphabetLow.find((item, i) => {  //  был alphabet стал alphabetLow
-         if (elem === i) return rotArr.push(item)
+      arrOfIndexPlusNum.forEach(elem => {
+         alphabet.find((item, i) => {  //  был alphabet стал alphabetLow
+            if (elem === i) return rotArr.push(item)
+         })
       })
-   })
+   }
+   message.split("").forEach(char => {
+      if (char == char.toUpperCase()) {
+         someFunction(alphabetUp, char)
+      } else if (char == char.toLowerCase()) {
+         someFunction(alphabetLow, char)
+      }
+   });
    return rotArr.join("")
 }
-console.log(rot13("Grfg"));
+console.log(rot13("GRFG"));
