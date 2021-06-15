272ceb54efc477cbac4b60c62df3e16f6dfd0700
Dmitriy <example@mail.com>
2021-06-15 14:21:30 +0300

    4. DONE (with comments)

--- script.js
+++ script.js
@@ -5,27 +5,31 @@ function rot13(message) {
    function someFunction(alphabet, elem) {
       let arrOfIndex = []  // массив чисел, соответствующих индексам в массиве alphabetLow/alphabetUp
       let arrOfIndexPlusNum  // те же числа но увеличенные на заданное число
-      let charIndex = alphabet.findIndex(item => item === elem)
-      arrOfIndex.push(charIndex)
-      arrOfIndexPlusNum = arrOfIndex.map(index => {
-         if ((index + 13) > alphabet.length) {
-            return (index + 13) - alphabet.length
+      let charIndex = alphabet.findIndex(item => item === elem) // находим индекс каждой буквы исх.строки в алфавите
+      arrOfIndex.push(charIndex) // добавляем в массив этих индексов
+      arrOfIndexPlusNum = arrOfIndex.map(index => { // преобразуем массив индексов на заданное число
+         if ((index + 13) >= alphabet.length) { // проверяем, если сумма индекса и заданного числа больше длины алфавита
+            return (index + 13) - alphabet.length // то возвращаем их разницу
          }
-         return index + 13
+         return index + 13 // или сумму индекса и заданного числа
       })
-      arrOfIndexPlusNum.forEach(elem => {
-         alphabet.find((item, i) => {  //  был alphabet стал alphabetLow
-            if (elem === i) return rotArr.push(item)
+      arrOfIndexPlusNum.forEach(elem => { // для преобразованного массива чисел ищем соответствия в алфавите 
+         alphabet.find((item, i) => {  // берем алфавит и каждый элемент и его индекс 
+            if (elem === i) return rotArr.push(item) // сравниваем индекс в алфавите и число из преобразованного массива: если равны, то пушим в итоговый массив
          })
       })
    }
-   message.split("").forEach(char => {
-      if (char == char.toUpperCase()) {
-         someFunction(alphabetUp, char)
-      } else if (char == char.toLowerCase()) {
-         someFunction(alphabetLow, char)
+   message.split("").forEach(char => { // разбиваем входящую строку на буквы, и к каждой выполняем код:
+      if (alphabetLow.includes(char) || alphabetUp.includes(char)) { // проверка на наличие символа в массивах
+         if (char == char.toUpperCase()) { // если буква большая
+            someFunction(alphabetUp, char) // то работаем с алфавитом в верхнем регистре
+         } else if (char == char.toLowerCase()) { // если буква маленькая
+            someFunction(alphabetLow, char) // то работаем с алфавитом в нижнем регистре
+         }
+      } else {
+         rotArr.push(char) // если символа в массивах нет, то просто пушим его в итоговый массив
       }
    });
-   return rotArr.join("")
+   return rotArr.join("") 
 }
-console.log(rot13("GRFG"));
+console.log(rot13("GRFG-+=435рол"));
